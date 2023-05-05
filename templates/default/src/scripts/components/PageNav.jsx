093f73c6938988ca3977690ba2f4390f6ee7fdee
Richard Niemand <niemand.richard@gmail.com>
2023-05-04 22:02:35 -0600

    Styles page menu

--- templates/default/src/scripts/components/PageNav.jsx
+++ templates/default/src/scripts/components/PageNav.jsx
@@ -1,25 +1,27 @@
 ((app) => {
   class PageNav extends React.Component {
-		constructor(props) {
-			super(props);
-		}
+    constructor(props) {
+      super(props);
+    }
 
-		render() {
+    render() {
       const pageHelper = this.props.pageHelper;
       const pages = pageHelper.allPages;
-      if(!pages || pages.length === 0) return null;
+      if (!pages || pages.length === 0) return null;
+      const activePageId = pageHelper.selectedPage?.id || 0;
 
-      return (<div className="post">
-        <ul>
-          {pages.map(page => {
-            return(<li key={page.slug} onClick={() => this.props.onPageSelected(page)}>
-              {page.title}
-            </li>);
-          })}
-        </ul>
-      </div>);
-		}
-	}
+      return (<ul className="page-nav">
+        {pages.map(page => {
+          let classes = '';
+          if (activePageId == page.id) classes += 'active';
+
+          return (<li key={page.slug} onClick={() => this.props.onPageSelected(page)}>
+            <a className={classes}>{page.title}</a>
+          </li>);
+        })}
+      </ul>);
+    }
+  }
 
   app.components.PageNav = PageNav;
 })(RnSSG);
